61d1f5f1d0becc8ffe882982d69101eb6733a5b7
Julia Alexeeva <julia4406@gmail.com>
2025-02-27 19:52:22 +0200

    Create: model for Book

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def book_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/movies/", filename)
+
+
+class Book(models.Model):
+
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=10, choices=CoverChoices.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField()
+    image = models.ImageField(null=True, upload_to=book_image_file_path)
--- backend/book/urls.py
+++ backend/book/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
 app_name = "book"
+
+router = routers.DefaultRouter()
+router.register("books", BookViewSet)
 
 urlpatterns = [path("", include(router.urls))]
