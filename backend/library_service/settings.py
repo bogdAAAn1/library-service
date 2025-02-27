8bc61b2b5b832a959a53eaa3358341c241285c51
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-27 22:13:10 +0200

    add drf_spectacular to INSTALLED_APPS

--- backend/library_service/settings.py
+++ backend/library_service/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     "borrowing",
     "payment",
     "user",
-    "rest_framework"
+    "rest_framework",
+    "drf_spectacular"
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,21 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Library Service API",
+    "DESCRIPTION": "Borrowing books from the library",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
