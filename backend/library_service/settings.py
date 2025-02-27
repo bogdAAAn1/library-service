93502683751dd06fbb8de703d0315088482cb19a
Maksym <chukhno.maksym@gmail.com>
2025-02-27 17:58:20 +0200

    Staring project. Creating new app for telegram Bot. Update settings and env.sample.

--- backend/library_service/settings.py
+++ backend/library_service/settings.py
@@ -23,6 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
@@ -45,7 +46,8 @@ INSTALLED_APPS = [
     "book",
     "borrowing",
     "payment",
-    "user"
+    "user",
+    "library_bot",
 ]
 
 MIDDLEWARE = [
