f81960f0dc054c547a357fe06bd6a60e657b59bb
Julia Alexeeva <julia4406@gmail.com>
2025-02-27 19:19:12 +0200

    Register: rest-framework and drf-spectacular app into settings.py

--- backend/library_service/settings.py
+++ backend/library_service/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "library_service",
+    "rest_framework",
+    "drf_spectacular",
     "book",
     "borrowing",
     "payment",
@@ -131,3 +133,20 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Library Service API",
+    "DESCRIPTION": "Borrowing books from the library",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
