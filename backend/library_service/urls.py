7ed3e8eacf061bf4a5ec6ac01c00c6e29b4db372
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-27 22:14:22 +0200

    add paths to documentation

--- backend/library_service/urls.py
+++ backend/library_service/urls.py
@@ -17,7 +17,18 @@ Including another URLconf
 
 # from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
