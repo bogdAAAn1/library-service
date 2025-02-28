a2046b3e8c8334623d0366f0563e15917d9f35c0
Maksym <chukhno.maksym@gmail.com>
2025-02-28 09:51:30 +0200

    Implement a simple cli task for celery to test notifications in telegram.

--- backend/book/tasks.py
+++ backend/book/tasks.py
@@ -1,8 +1,19 @@
-from celery import shared_task
+import asyncio
+import os
 
+from celery import shared_task
+from dotenv import load_dotenv
+from telegram import Bot
 
 @shared_task
-def add():
-    x = 2
-    y = 2
-    return x + y
+def send_notification_to_telegram():
+    load_dotenv()
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = "-1002269497388"
+    message = "TestMessage"
+
+    async def send_message():
+        bot = Bot(token=token)
+        await bot.send_message(chat_id=chat_id, text=message)
+
+    asyncio.run(send_message())
