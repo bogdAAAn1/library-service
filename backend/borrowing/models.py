7e82c28f711ba6440d8e13259ab5f1df5ac587c6
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-03-03 14:00:55 +0200

    update borrowings for payment system

--- backend/borrowing/models.py
+++ backend/borrowing/models.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.db import models
 
 from book.models import Book
+from payment.models import Payment
+
+
+FINE_MULTIPLIER = 2
+
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
@@ -17,3 +22,36 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings"
     )
+
+    def get_payment_type(self):
+        """return payment type"""
+        if self.actual_return_date and self.actual_return_date > self.expected_return_date:
+            return Payment.PaymentType.FINE
+        return Payment.PaymentType.PAYMENT
+
+    def get_payment_message(self):
+        """return payment message"""
+        if self.get_payment_type() == Payment.PaymentType.FINE:
+            return "The book is returned overdue. You need to pay a fine."
+        return "The book is returned on time. Please pay the rental fee."
+
+    def get_total_rental_fee(self):
+        """return total rental fee"""
+        if self.actual_return_date and self.borrow_date:
+            rental_days = (self.actual_return_date - self.borrow_date).days
+            return max(rental_days, 1) * self.book.daily_fee
+        return self.book.daily_fee
+
+    def get_late_fee(self):
+        """return late fee"""
+        if self.actual_return_date and self.expected_return_date:
+            if self.actual_return_date > self.expected_return_date:
+                overdue_days = (self.actual_return_date - self.expected_return_date).days
+                return overdue_days * self.book.daily_fee * FINE_MULTIPLIER
+        return 0
+
+    def get_total_payment(self):
+        """return total payment"""
+        rental_fee = self.get_total_rental_fee()
+        late_fee = self.get_late_fee()
+        return rental_fee + late_fee
