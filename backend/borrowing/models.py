79e193e193df4928cafc002a3e7c6a0338b28374
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-03-03 18:59:09 +0200

    refactor code

--- backend/borrowing/models.py
+++ backend/borrowing/models.py
@@ -58,5 +58,6 @@ class Borrowing(models.Model):
 
     def __str__(self) -> str:
         return (
-            f"Borrowing by {self.user} - Book: {self.book.title} on {self.borrow_date}"
+            f"Borrowing by {self.user} - "
+            f"Book: {self.book.title} on {self.borrow_date}"
         )
