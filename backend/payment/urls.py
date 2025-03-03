3bec4634fe66b847682dbcfcdfa703b9a5a0cbd0
Julia Alexeeva <julia4406@gmail.com>
2025-03-03 17:02:09 +0200

    Modify: 'payment' prefix removed

--- backend/payment/urls.py
+++ backend/payment/urls.py
@@ -10,7 +10,7 @@ from payment.views import (
 app_name = "payment"
 
 router = DefaultRouter()
-router.register("payment", PaymentViewSet, basename="payment")
+router.register("", PaymentViewSet, basename="payment")
 
 urlpatterns = [
     path("", include(router.urls)),
