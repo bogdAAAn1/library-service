d00b944e869b22d0ceeded1181a8a425fcbf3152
Julia Alexeeva <julia4406@gmail.com>
2025-03-03 17:29:20 +0200

    Modify: return image-upload (rollback)

--- backend/book/views.py
+++ backend/book/views.py
@@ -1,11 +1,9 @@
-from typing import List
-
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import mixins, status
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import IsAdminUser, BasePermission
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -26,7 +24,7 @@ class BookViewSet(
     GenericViewSet,
 ):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ["title", "cover", "inventory"]
     filterset_class = BookFilter
@@ -34,7 +32,31 @@ class BookViewSet(
     ordering = ["id"]
     permission_classes = [IsAdminUser]
 
-    def get_permissions(self) -> List[BasePermission]:
+    def get_permissions(self):
         if self.action in ["list", "retrieve"]:
             return []
         return [permission() for permission in self.permission_classes]
+
+    def get_serializer_class(self):
+
+        if self.action == "upload_image":
+            return BookImageSerializer
+
+        return BookSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image of cover to book"""
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
